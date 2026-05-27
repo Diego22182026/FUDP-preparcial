87bfca1404f6d3371d065f74e6241f9202cda0f8
Diego22182026 <00242126@uca.edu.sv>
2026-05-27 10:02:33 -0600

    feat: edades.cpp and 2compilado.exe

--- Repasoparcial2/edades.cpp
+++ Repasoparcial2/edades.cpp
@@ -4,8 +4,28 @@ using namespace std;
 int main (){
 
     int edad;
-    
 
+    cout << "Por favor ingrese su edad: ";
+    cin >> edad;
+
+    if (edad <18 && edad >=0){
+
+        cout << "Usted es menor de edad" << endl;
+    }
+
+    else if (edad >=18 && edad <=59){
+
+        cout << "Usted es un adulto" << endl;
+    }
+
+    else if (edad >=60){
+
+        cout << "Usted es un adulto mayor" << endl;
+    }
+
+    else
+
+        cout << "Edad invalida, por favor intente de nuevo." << endl;
 
 
     return 0;
