3554ddd7470009f32cebb7df1c36bf54d1edf3c0
Diego22182026 <00242126@uca.edu.sv>
2026-05-27 09:57:04 -0600

    update: slarios.cpp

--- Repasoparcial2/salarios.cpp
+++ Repasoparcial2/salarios.cpp
@@ -8,15 +8,17 @@ float salario, horas;
 cout << "Hola, por favor ingresa cuanto te pagan por hora y las horas que trabajas: " << endl;
 cin >> salario >> horas;
 
-if (horas >= 40){
+if (horas >= 40 && horas > 0){
     salario = salario * 1.5;
 
     cout << "Tu salario total es de: " << salario << " " << "dolares la hora" << endl; 
 }
 
-else if (horas <=39){
-
+else if (horas <=39 && horas > 0){
     cout << "Tu salario total es de: " << salario << " " << "dolares la hora" << endl;
 }
+
+else 
+    cout << "Datos invalidos" << endl;
     return 0;
 }
